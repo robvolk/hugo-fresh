695484a3f7fbadeb0bda5dfd73cde02933b6cdbd
rob <Rob Volk>
2018-05-28 23:34:48 -0500

    Remove preloader javascript

--- source/js/fresh.js
+++ source/js/fresh.js
@@ -55,18 +55,6 @@ $(function() {
     return false;
   });
 
-  //Preloader
-  $(window).on("load", function() {
-    // makes sure the whole site is loaded
-    $("#status").fadeOut(); // will first fade out the loading animation
-    $("#preloader")
-      .delay(350)
-      .fadeOut("slow"); // will fade out the white DIV that covers the website.
-    $("body")
-      .delay(350)
-      .css({ overflow: "visible" });
-  });
-
   //Left Sidebar init and ps-scrollbar init
   if ($("#panel-trigger").length) {
     $("#panel-trigger, #panel-close").panelslider({
